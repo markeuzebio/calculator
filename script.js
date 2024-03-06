3bd076798a97df862a80ba3d5323ee87777a4123
Marcos Euzébio <markeuzebio@gmail.com>
2024-03-06 15:36:44 -0300

    Add behavior to "backspace" and "clear" buttons
    When clicked, the "backspace" button pop out the
    last number written on display.
    
    When clicked, the "clear" button replaces
    everything on display to value 0.
    
    The number on display has been sized to be bigger
    than before. This way the result can be seen more
    easily.
    
    Some variable names on script.js was changed to
    add the "btn" prefix; it is easier to identify
    each variable purpose this way.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const display_value       = document.querySelector(".display");
-const calculator_numbers  = document.querySelectorAll(".number");
+const btn_numbers         = document.querySelectorAll(".number");
+const btn_backspace       = document.querySelector("#backspace");
+const btn_clear           = document.querySelector("#clear");
 
 function add(a, b)
 {
@@ -46,13 +48,32 @@ function operate(a, operator, b)
     }
 }
 
-function updateDisplay(number) {
+function addNumberDisplay(number)
+{
     if(display_value.textContent === "0")
         display_value.textContent = number;
     else
         display_value.textContent += number;
 }
 
-calculator_numbers.forEach(function (calculator_number) {
-    calculator_number.addEventListener("click", () => updateDisplay(calculator_number.textContent));
+function removeLastNumberDisplay()
+{
+    if(display_value.textContent !== "0")
+    {
+        display_value.textContent = display_value.textContent.slice(0, -1);
+
+        if(display_value.textContent.length == 0)
+            display_value.textContent = 0;
+    }
+}
+
+// Update display based on number clicked on calculator
+btn_numbers.forEach(function (btn_number) {
+    btn_number.addEventListener("click", () => addNumberDisplay(btn_number.textContent));
 });
+
+// Pop the last number written on display
+btn_backspace.addEventListener("click", removeLastNumberDisplay);
+
+// Clear the display
+btn_clear.addEventListener("click", () => display_value.textContent = 0);
